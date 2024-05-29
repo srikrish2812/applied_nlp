87ba3922456f00cd22b2ce416abf3326030d3a1d
abhay <abhay2001sri@gmail.com>
2024-05-30 00:34:46 +0530

    minor changes

---
+++ week_1/install_via_conda.sh
@@ -0,0 +1,7 @@
+conda install -y ipython==8.5.0
+conda install -y jupyter==1.0.0
+conda install -y nbimporter==0.3.4
+conda install -y pandas==1.3.5
+conda install -y spacy==3.2.4
+conda install -y bs4==0.0.1
+conda install -y pytest==7.1.3
